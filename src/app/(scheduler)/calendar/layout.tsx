9f50744116b6c789a698d6857cc0fad38f1fb1b4
minyoung <uperxwall@gmail.com>
2024-07-09 20:44:34 +0900

    [Del] todo, calendar layout 삭제

--- src/app/(scheduler)/calendar/layout.tsx
+++
@@ -1,7 +0,0 @@
-import { PropsWithChildren } from "react";
-
-const CalendarLayout = ({ children }: PropsWithChildren) => {
-  return <section>{children}</section>;
-};
-
-export default CalendarLayout;
