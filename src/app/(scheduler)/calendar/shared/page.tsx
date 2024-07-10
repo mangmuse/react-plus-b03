fbaea5fba6937f12e3d1b83ce161c73af4dcc681
show1486 <show1486@naver.com>
2024-07-10 23:59:03 +0900

    feat:공유페이지 초안

--- src/app/(scheduler)/calendar/shared/page.tsx
+++ src/app/(scheduler)/calendar/shared/page.tsx
@@ -1,13 +1,7 @@
-import Page from "@/components/Page";
-import TodoItem from "../../_components/TodoItem/TodoItem";
+import Section from "@/components/Section";
 
 const SharedCalendarPage = () => {
-  return (
-    <Page title="공유 캘린더" isTitleHidden={true}>
-      공유 캘린더 페이지
-      <TodoItem />
-    </Page>
-  );
+  return <Section />;
 };
 
 export default SharedCalendarPage;
