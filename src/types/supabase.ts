d338966e593aa0ae3b37fae7ce9998bd06f74df2
mangmuse <mangmangmuse@gmail.com>
2024-07-12 22:36:24 +0900

    [Update] supabase types

--- src/types/supabase.ts
+++ src/types/supabase.ts
@@ -6,36 +6,254 @@ export type Json =
   | { [key: string]: Json | undefined }
   | Json[];
 
-  export type Database = {
-    public: {
-      Tables: {
-        users: {
-          Row: {
-            email: string
-            id: string
-            nickname: string | null
+export type Database = {
+  public: {
+    Tables: {
+      calendars: {
+        Row: {
+          createdAt: string;
+          description: string;
+          id: string;
+          name: string;
+          ownerId: string;
+        };
+        Insert: {
+          createdAt?: string;
+          description?: string;
+          id?: string;
+          name?: string;
+          ownerId: string;
+        };
+        Update: {
+          createdAt?: string;
+          description?: string;
+          id?: string;
+          name?: string;
+          ownerId?: string;
+        };
+        Relationships: [
+          {
+            foreignKeyName: "calendars_owner_id_fkey";
+            columns: ["ownerId"];
+            isOneToOne: false;
+            referencedRelation: "users";
+            referencedColumns: ["id"];
           }
-          Insert: {
-            email: string
-            id?: string
-            nickname?: string | null
+        ];
+      };
+      comments: {
+        Row: {
+          calendarId: string;
+          content: string;
+          createdAt: string;
+          id: string;
+          userId: string;
+        };
+        Insert: {
+          calendarId: string;
+          content: string;
+          createdAt?: string;
+          id?: string;
+          userId: string;
+        };
+        Update: {
+          calendarId?: string;
+          content?: string;
+          createdAt?: string;
+          id?: string;
+          userId?: string;
+        };
+        Relationships: [
+          {
+            foreignKeyName: "comments_calendarId_fkey";
+            columns: ["calendarId"];
+            isOneToOne: false;
+            referencedRelation: "calendars";
+            referencedColumns: ["id"];
+          },
+          {
+            foreignKeyName: "comments_user_id_fkey";
+            columns: ["userId"];
+            isOneToOne: false;
+            referencedRelation: "users";
+            referencedColumns: ["id"];
           }
-          Update: {
-            email?: string
-            id?: string
-            nickname?: string | null
+        ];
+      };
+      default_calendars: {
+        Row: {
+          createdAt: string;
+          id: string;
+          userId: string;
+        };
+        Insert: {
+          createdAt?: string;
+          id?: string;
+          userId: string;
+        };
+        Update: {
+          createdAt?: string;
+          id?: string;
+          userId?: string;
+        };
+        Relationships: [
+          {
+            foreignKeyName: "default_calendars_userId_fkey";
+            columns: ["userId"];
+            isOneToOne: false;
+            referencedRelation: "users";
+            referencedColumns: ["id"];
           }
-          Relationships: [
-            {
-              foreignKeyName: "users_id_fkey"
-              columns: ["id"]
-              isOneToOne: true
-              referencedRelation: "users"
-              referencedColumns: ["id"]
-            },
-          ]
-        }
-      }
+        ];
+      };
+      default_todos: {
+        Row: {
+          calendarId: string;
+          createdAt: string;
+          description: string | null;
+          endDate: string | null;
+          id: string;
+          startDate: string | null;
+          title: string;
+        };
+        Insert: {
+          calendarId: string;
+          createdAt?: string;
+          description?: string | null;
+          endDate?: string | null;
+          id?: string;
+          startDate?: string | null;
+          title: string;
+        };
+        Update: {
+          calendarId?: string;
+          createdAt?: string;
+          description?: string | null;
+          endDate?: string | null;
+          id?: string;
+          startDate?: string | null;
+          title?: string;
+        };
+        Relationships: [
+          {
+            foreignKeyName: "default_todos_calendarId_fkey";
+            columns: ["calendarId"];
+            isOneToOne: false;
+            referencedRelation: "default_calendars";
+            referencedColumns: ["id"];
+          }
+        ];
+      };
+      participants: {
+        Row: {
+          calendarId: string;
+          createdAt: string;
+          id: string;
+          userId: string;
+        };
+        Insert: {
+          calendarId: string;
+          createdAt?: string;
+          id?: string;
+          userId: string;
+        };
+        Update: {
+          calendarId?: string;
+          createdAt?: string;
+          id?: string;
+          userId?: string;
+        };
+        Relationships: [
+          {
+            foreignKeyName: "participants_calendar_id_fkey";
+            columns: ["calendarId"];
+            isOneToOne: false;
+            referencedRelation: "calendars";
+            referencedColumns: ["id"];
+          },
+          {
+            foreignKeyName: "participants_user_id_fkey";
+            columns: ["userId"];
+            isOneToOne: false;
+            referencedRelation: "users";
+            referencedColumns: ["id"];
+          }
+        ];
+      };
+      todos: {
+        Row: {
+          calendarId: string;
+          createdAt: string;
+          description: string | null;
+          endDate: string | null;
+          id: string;
+          isDone: boolean;
+          isImportant: boolean;
+          startDate: string | null;
+          title: string;
+        };
+        Insert: {
+          calendarId: string;
+          createdAt?: string;
+          description?: string | null;
+          endDate?: string | null;
+          id?: string;
+          isDone?: boolean;
+          isImportant?: boolean;
+          startDate?: string | null;
+          title?: string;
+        };
+        Update: {
+          calendarId?: string;
+          createdAt?: string;
+          description?: string | null;
+          endDate?: string | null;
+          id?: string;
+          isDone?: boolean;
+          isImportant?: boolean;
+          startDate?: string | null;
+          title?: string;
+        };
+        Relationships: [
+          {
+            foreignKeyName: "todos_calendar_id_fkey";
+            columns: ["calendarId"];
+            isOneToOne: false;
+            referencedRelation: "calendars";
+            referencedColumns: ["id"];
+          }
+        ];
+      };
+      users: {
+        Row: {
+          email: string;
+          id: string;
+          image_url: string | null;
+          nickname: string;
+        };
+        Insert: {
+          email: string;
+          id?: string;
+          image_url?: string | null;
+          nickname: string;
+        };
+        Update: {
+          email?: string;
+          id?: string;
+          image_url?: string | null;
+          nickname?: string;
+        };
+        Relationships: [
+          {
+            foreignKeyName: "users_id_fkey";
+            columns: ["id"];
+            isOneToOne: true;
+            referencedRelation: "users";
+            referencedColumns: ["id"];
+          }
+        ];
+      };
+    };
     Views: {
       [_ in never]: never;
     };
--- src/utils/supabase/server.ts
+++ src/utils/supabase/server.ts
@@ -1,23 +1,26 @@
 // utils/supabase/server.ts
 
-import { createServerClient } from '@supabase/ssr'
-import { cookies } from 'next/headers'
+import { Database } from "@/types/supabase";
+import { createServerClient } from "@supabase/ssr";
+import { cookies } from "next/headers";
 
 export function createClient() {
-  const cookieStore = cookies()
+  const cookieStore = cookies();
 
-  return createServerClient(
+  return createServerClient<Database>(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_KEY!,
     {
       cookies: {
         getAll() {
-          return cookieStore.getAll()
+          return cookieStore.getAll();
         },
         setAll(cookiesToSet) {
-          cookiesToSet.forEach(({ name, value, options }) => cookieStore.set(name, value, options))
+          cookiesToSet.forEach(({ name, value, options }) =>
+            cookieStore.set(name, value, options)
+          );
         },
       },
     }
-  )
+  );
 }
