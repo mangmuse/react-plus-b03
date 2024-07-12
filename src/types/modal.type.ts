8b6ca6b9355af21cee772c2435b612478312eccc
fjw1010 <fjw_1010@naver.com>
2024-07-13 02:18:58 +0900

    [fix] 일정보기 ui 완료

--- src/types/modal.type.ts
+++ src/types/modal.type.ts
@@ -1,5 +1,5 @@
 export interface ModalProps {
-  type: "todo" | "confirm" | "alert" | "Edit" | "Modify";
+  type: "todo" | "confirm" | "alert" | "Edit" | "Modify" | "form";
   content: string;
   onConfirm?: () => void;
   onCancel?: () => void;
