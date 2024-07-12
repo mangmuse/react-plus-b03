1f5a0edc57bcdc78858404515a4ad610e7529736
mangmuse <mangmangmuse@gmail.com>
2024-07-12 20:47:49 +0900

    [Add] POST calendar route handler

---
+++ src/app/api/calendar/route.ts
@@ -0,0 +1,26 @@
+import { NextRequest, NextResponse } from "next/server";
+import { createClient } from "@/utils/supabase/server";
+
+export async function POST(req: NextRequest) {
+  const supabase = createClient();
+
+  const {
+    data: { user },
+    error: userError,
+  } = await supabase.auth.getUser();
+  if (user && !userError) {
+    const { id: userId } = user;
+    const newCalendar = {
+      ownerId: userId,
+    };
+
+    const { status: insertStatus } = await supabase
+      .from("calendars")
+      .insert(newCalendar)
+      .single();
+
+    return NextResponse.json(insertStatus);
+  }
+
+  return NextResponse.json(";ㅅ;", { status: 500 });
+}
