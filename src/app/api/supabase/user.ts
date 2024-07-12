ed9183cd90744754428e7e2800f600067949a072
ilovezerocokeya <147008077a@gmail.com>
2024-07-12 14:35:43 +0900

    [update] 로그인/주스탠드

--- src/app/api/supabase/user.ts
+++ src/app/api/supabase/user.ts
@@ -1,23 +1,39 @@
-// app/api/supabase/user.ts
-
-import { createClient } from "@/utils/supabase/client";
-
 // supabase auth server에서 로그인 유저 확인
+
+import { getSupabaseClient } from "@/utils/supabase/client";
+
 // 신뢰도 높음
 export async function getCurrentUser() {
+  const supabase = getSupabaseClient();
   const {
     data: { user },
     error,
-  } = await createClient().auth.getUser();
+  } = await supabase.auth.getUser();
   return { user, error };
 }
 
 // 로컬 스토리지 혹은 쿠키에서 로그인 유저 확인
 // 신뢰도 낮음
 export async function getCurrentSession() {
+  const supabase = getSupabaseClient();
   const {
     data: { session },
     error,
-  } = await createClient().auth.getSession();
+  } = await supabase.auth.getSession();
   return { session, error };
 }
+
+// user 테이블 값 가져오기
+export const getUserData = async (email: string) => {
+  const supabase = getSupabaseClient();
+  return await supabase
+  .from("users")
+  .select("id, email, nickname")
+  .eq("email", email)
+  .single();
+};
+
+
+
+
+
--- src/app/auth/signup/page.tsx
+++ src/app/auth/signup/page.tsx
@@ -97,76 +97,78 @@ export default function SignUpPage() {
 	};
 
 	return (
-		<form
-			onSubmit={onSubmit}
-			className="flex flex-col gap-y-2 border-2 w-96 items-center"
-		>
-			<div className="w-full flex items-center justify-between p-4">
-				<label htmlFor="email" className="mr-2">
-					이메일
-				</label>
-				<input
-					type="email"
-					id="email"
-					value={email}
-					onChange={onChangeEmail}
-					placeholder="이메일을 입력하세요."
-					className="border-2 border-gray-300 rounded-md p-1"
-				/>
-			</div>
-			<div className="w-full flex items-center justify-between p-4">
-				<label htmlFor="nickname" className="mr-2">
-					닉네임
-				</label>
-				<input
-					type="text"
-					id="nickname"
-					value={nickname}
-					onChange={onChangeNickname}
-					placeholder="닉네임을 입력하세요."
-					className="border-2 border-gray-300 rounded-md p-1"
-				/>
-			</div>
-			<div className="w-full flex items-center justify-between p-4">
-				<label htmlFor="password" className="mr-2">
-					비밀번호
-				</label>
-				<input
-					type="password"
-					id="password"
-					value={password}
-					onChange={onChangePassword}
-					placeholder="비밀번호를 입력하세요."
-					className="border-2 border-gray-300 rounded-md p-1"
-				/>
-			</div>
-			{error.password && <p className="text-red-500">{error.password}</p>}
-			<div className="w-full flex items-center justify-between p-4">
-				<label htmlFor="passwordConfirm" className="mr-2">
-					비밀번호 확인
-				</label>
-				<input
-					type="password"
-					id="passwordConfirm"
-					value={passwordConfirm}
-					onChange={onChangePasswordConfirm}
-					placeholder="비밀번호를 입력하세요."
-					className="border-2 border-gray-300 rounded-md p-1"
-				/>
-			</div>
-			{error.passwordConfirm && (
-				<p className="text-red-500">{error.passwordConfirm}</p>
-			)}
-			<div className="w-full p-4 flex flex-col gap-y-2">
-				<button className="w-full bg-blue-500 text-white p-2 rounded-md">
-					회원가입
-				</button>
-				<Link href="/auth/login">
-					<button type="button" className="w-full bg-black text-white p-2 rounded-md">
-						로그인하러 가기
+		<div className="flex items-center justify-center min-h-screen">
+			<form
+				onSubmit={onSubmit}
+				className="flex flex-col gap-y-2 border-2 w-96 items-center p-4 bg-white rounded shadow-md"
+			>
+				<div className="w-full flex items-center justify-between mb-4">
+					<label htmlFor="email" className="mr-2">
+						이메일
+					</label>
+					<input
+						type="email"
+						id="email"
+						value={email}
+						onChange={onChangeEmail}
+						placeholder="이메일을 입력하세요."
+						className="border-2 border-gray-300 rounded-md p-1 w-full"
+					/>
+				</div>
+				<div className="w-full flex items-center justify-between mb-4">
+					<label htmlFor="nickname" className="mr-2">
+						닉네임
+					</label>
+					<input
+						type="text"
+						id="nickname"
+						value={nickname}
+						onChange={onChangeNickname}
+						placeholder="닉네임을 입력하세요."
+						className="border-2 border-gray-300 rounded-md p-1 w-full"
+					/>
+				</div>
+				<div className="w-full flex items-center justify-between mb-4">
+					<label htmlFor="password" className="mr-2">
+						비밀번호
+					</label>
+					<input
+						type="password"
+						id="password"
+						value={password}
+						onChange={onChangePassword}
+						placeholder="비밀번호를 입력하세요."
+						className="border-2 border-gray-300 rounded-md p-1 w-full"
+					/>
+				</div>
+				{error.password && <p className="text-red-500">{error.password}</p>}
+				<div className="w-full flex items-center justify-between mb-4">
+					<label htmlFor="passwordConfirm" className="mr-2">
+						비밀번호 확인
+					</label>
+					<input
+						type="password"
+						id="passwordConfirm"
+						value={passwordConfirm}
+						onChange={onChangePasswordConfirm}
+						placeholder="비밀번호를 입력하세요."
+						className="border-2 border-gray-300 rounded-md p-1 w-full"
+					/>
+				</div>
+				{error.passwordConfirm && (
+					<p className="text-red-500">{error.passwordConfirm}</p>
+				)}
+				<div className="w-full p-4 flex flex-col gap-y-2">
+					<button className="w-full bg-blue-500 text-white p-2 rounded-md">
+						회원가입
 					</button>
-				</Link>
-			</div>
-		</form>
+					<Link href="/auth/login">
+						<button type="button" className="w-full bg-black text-white p-2 rounded-md">
+							로그인하러 가기
+						</button>
+					</Link>
+				</div>
+			</form>
+		</div>
 	);
 }
