73240c5a606cf653781b762a9bc3330a0e86c457
show1486 <show1486@naver.com>
2024-07-12 15:29:54 +0900

    feat:모달추가

---
+++ src/components/Modal/DeleteModal.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import Button from "../Button";
+import { ModalProps } from "@/types/modal.type";
+
+const DeleteModal = ({ type, content, onClose }: ModalProps) => {
+  return (
+    <div className="w-80 h-40 pt-9 px-14 pb-9 bg-[#fff] flex flex-col justify-center items-center">
+      <div className="text-center text-xl font-bold">{content}</div>
+      <div className="flex w-full mt-8 justify-center gap-2">
+        <Button className="rounded-2xl bg-[#1c1d22] " onClick={onClose}>
+          확인
+        </Button>
+        <Button
+          className=" rounded-2xl border-2 border-solid bg-[#fff] text-[#000]"
+          onClick={onClose}
+        >
+          취소
+        </Button>
+      </div>
+    </div>
+  );
+};
+
+export default DeleteModal;
