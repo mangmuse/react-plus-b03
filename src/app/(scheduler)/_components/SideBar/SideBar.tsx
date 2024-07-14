da2a4bc5c565974a8390ec3f136da027b432691a
fjw1010 <fjw_1010@naver.com>
2024-07-14 19:54:13 +0900

    fix : 사이드바 마이페이지 ui 완료

--- src/app/(scheduler)/_components/SideBar/SideBar.tsx
+++ src/app/(scheduler)/_components/SideBar/SideBar.tsx
@@ -24,11 +24,11 @@ const SideBar = () => {
           </p>
           <div className="ml-4">
             <p className="flex flex-col">
-              <p className="font-semibold text-center">유저 정보</p>
               <p className="flex items-center gap-1">
                 <Image src="/icons/sidebar/ic-user-page.png" alt="" width={17} height={10} />
                 <Link href="">마이페이지</Link>
               </p>
+              <p className="font-semibold text-center">유저 정보</p>
             </p>
           </div>
         </div>
